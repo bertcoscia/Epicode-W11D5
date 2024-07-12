62b9677ca57f08432b92f5e264022de38419c2b4
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-12 12:01:16 +0200

    adds Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,6 +12,30 @@ const Main = () => {
           <a href="#Home">DISCOVER</a>
         </Col>
       </Row>
+      <Row>
+        <Col xs={10}>
+          <div id="rock">
+            <h2>Rock Classics</h2>
+            <Row id="rockSection" className="row-cols-1 row-cols-sm-2 row-cols-lg3 row-cols-xl-4 imgLinks py-3"></Row>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10}>
+          <div id="pop">
+            <h2>Pop Culture</h2>
+            <Row id="popSection" className="row-cols-1 row-cols-sm-2 row-cols-lg3 row-cols-xl-4 imgLinks py-3"></Row>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10}>
+          <div id="hiphop">
+            <h2>#HipHop</h2>
+            <Row id="hipHopSection" className="row-cols-1 row-cols-sm-2 row-cols-lg3 row-cols-xl-4 imgLinks py-3"></Row>
+          </div>
+        </Col>
+      </Row>
     </Col>
   );
 };
