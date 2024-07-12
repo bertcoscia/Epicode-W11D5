ab02633231964af5b76922df3ebb4649076b3617
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-12 18:29:47 +0200

    adds form reset on submit

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -15,6 +15,7 @@ const Aside = () => {
   const handleSubmit = event => {
     event.preventDefault();
     dispatch(getSongsAction(query));
+    setQuery("");
   };
 
   return (
@@ -44,7 +45,7 @@ const Aside = () => {
                 </Nav.Item>
                 <Form onSubmit={handleSubmit}>
                   <InputGroup className="mt-3">
-                    <Form.Control type="text" placeholder="Search" aria-label="Search" onChange={handleChange} />
+                    <Form.Control value={query} type="text" placeholder="Search" aria-label="Search" onChange={handleChange} />
                     <Button variant="dark" className="btn-outline-secondary btn-sm h100">
                       GO
                     </Button>
@@ -65,49 +66,3 @@ const Aside = () => {
 };
 
 export default Aside;
-
-{
-  /* <Col md={2}>
-      <Nav id="sidebar" className="navbar navbar-expand-md fixed-left justify-content-between">
-        <Container className="flex-column align-items-start">
-          <Navbar.Brand href="#Home">
-            <img src="./assets/logo/logo.png" alt="Spotify logo" style={{ width: "131px", height: "40px" }} />
-          </Navbar.Brand>
-          <Button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </Button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <Navbar>
-              <Nav className="d-flex flex-column row-gap-3">
-                <Nav.Item>
-                  <Nav.Link href="#Home" className="d-flex align-items-centre d-block">
-                    <HouseDoorFill size={25} />
-                    &nbsp; Home
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#Home" className="d-flex align-items-centre d-block">
-                    <BookFill size={25} />
-                    &nbsp; Your Library
-                  </Nav.Link>
-                </Nav.Item>
-                <Form onSubmit={handleSubmit}>
-                  <InputGroup className="mt-3">
-                    <Form.Control type="text" placeholder="Search" aria-label="Search" onChange={handleChange} />
-                    <Button variant="dark" className="btn-outline-secondary btn-sm h100">
-                      GO
-                    </Button>
-                  </InputGroup>
-                </Form>
-              </Nav>
-            </Navbar>
-          </div>
-        </Container>
-        <div className="nav-btn">
-          <Button className="signup-btn">Sign Up</Button>
-          <Button className="login-btn">Login</Button>
-          <a href="#Home">Cookie Policy</a> | <a href="#Home">Privacy</a>
-        </div>
-      </Nav>
-    </Col> */
-}
