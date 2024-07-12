bd97acce3349ef8d5ec9e86163203f0412fdee2d
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-12 09:48:13 +0200

    initialises repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/store/index.js";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
